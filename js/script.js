00fef46062181709fe4d5347cf6d4f42d38105b6
Ayat Elsherif <ayat_elsherif@yahoo.com>
2020-06-18 16:57:05 +0200

    change theme color in mobile, adding wowJS

--- js/script.js
+++ js/script.js
@@ -49,55 +49,6 @@ $(function(){
   });
 /**************offcanvas *************/
 
-    $('#homeBlog').owlCarousel({
-        loop:true,
-        dots:false,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            768:{
-                items:1
-            },
-            992:{
-                items:1
-            },
-            1200:{
-                items:2
-            }
-        }
-    })
-    $('#othersArticle').owlCarousel({
-      loop:true,
-      dots:false,
-      nav:true,
-      responsive:{
-          0:{
-              items:1
-          },
-          768:{
-              items:1
-          },
-          992:{
-              items:1
-          },
-          1200:{
-              items:2
-          }
-      }
-  })
-
-    $('[data-toggle="tooltip"]').tooltip({
-        
-        title:"Mon-Fri <br> 8:00AM-6:00PM <br>(GMT +2)",
-        html:true
-    });
-
-    // $('#navbarSupportedContent .nav-item').click(function(){
-    //   $('#navbarSupportedContent').removeClass('show');
-    // });
-
     new WOW().init();
 
 });
